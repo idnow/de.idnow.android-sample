91d735f605f5439202611248b1bab75527ab2409
Holger Kretzschmar <holger.kretzschmar@idnow.de>
2017-06-19 15:51:12 +0200

    udpated sample app to use SDK 3.1.3

--- app/src/main/java/de/idnow/sampleproject/MainActivity.java
+++ app/src/main/java/de/idnow/sampleproject/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends Activity
 				try
 				{
 					IDnowSDK.getInstance().initialize( MainActivity.this, "ihrebank" );
-					IDnowSDK.setTransactionToken( "DEV-UKTXA", context );
+					IDnowSDK.setTransactionToken( "DEV-SDJKV", context );
 					IDnowSDK.setShowVideoOverviewCheck( true, context );
 					IDnowSDK.setShowErrorSuccessScreen( true, context );
 
